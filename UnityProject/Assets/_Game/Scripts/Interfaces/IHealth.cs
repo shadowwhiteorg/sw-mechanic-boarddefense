3d98f0d2f14ad80d30b91487a715bc59b35114eb
ww-org <semir@shadowwhite.org>
2025-09-04 14:57:53 +0300

    Feat: Add HealthPlugin and IHealth interface for character health management

---
+++ UnityProject/Assets/_Game/Scripts/Interfaces/IHealth.cs
@@ -0,0 +1,9 @@
+﻿namespace _Game.Interfaces
+{
+    public interface IHealth : ICharacterPlugin
+    {
+        int Current { get; }
+        int Max { get; }
+        void ApplyDamage(int dmg);
+    }
+}
---
+++ UnityProject/Assets/_Game/Scripts/Runtime/Characters/Plugins/HealthPlugin.cs
@@ -0,0 +1,34 @@
+﻿using _Game.Interfaces;
+using _Game.Runtime.Characters;
+
+namespace _Game.Runtime.Characters.Plugins
+{
+    public sealed class HealthPlugin : IHealth
+    {
+        private CharacterEntity _entity;
+        public int Current { get; private set; }
+        public int Max { get; }
+
+        public HealthPlugin(int max)
+        {
+            Max = max < 1 ? 1 : max;
+            Current = Max;
+        }
+
+        public void OnSpawn(CharacterEntity e) { _entity = e; }
+        public void Tick(float dt) { }
+
+        public void ApplyDamage(int dmg)
+        {
+            if (dmg <= 0) return;
+            Current -= dmg;
+            if (Current <= 0)
+            {
+                Current = 0;
+                // TODO: fire CharacterDiedEvent & return entity to pool via a Despawn flow
+            }
+        }
+
+        public void OnDespawn() { _entity = null; }
+    }
+}
